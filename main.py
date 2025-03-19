1b583a8d8c5709e7c601fddec7ca8e01a493af60
Jose Sousa <2409jmsousa@gmail.com>
2025-03-19 09:01:43 +0000

    Tweaks

--- main.py
+++ main.py
@@ -184,9 +184,6 @@ class JellyFieldState:
                                     if color not in ['E', 'N']:
                                         changes = True
                                         globalCollisionColors.add(color)
-                                        self.goal[color] = max(self.goal[color] - 1, 0)
-                                        self.board[ni][nj].erase(color)
-                                        self.board[ni][nj].expand()
                                         
                     for color in globalCollisionColors:
                         self.board[i][j].erase(color)
